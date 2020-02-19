25c7690d7dc4d8a192487967b8740a9ab5dd7498
Hosward Villoria <hosward.avc@gmail.com>
2020-02-19 17:47:23 -0300

    creates all squares after a board is created

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,7 +1,22 @@
 class Board < ApplicationRecord
-  MAX_CELLS_QUANTITY = 81
+  MAX_LENGTH_QUANTITY = 9
   
   belongs_to :game
   has_many :squares
 
+  after_create :generate_squares
+
+  def generate_squares
+    MAX_LENGTH_QUANTITY.times do |x|
+      MAX_LENGTH_QUANTITY.times do |y|
+        Square.create(x_pos: x, y_pos: y, board: self)
+      end
+    end
+  end
+
+  private
+
+  def mines_left
+    @mines_left = 10
+  end
 end
